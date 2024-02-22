c415266119b39f7a74289c6838615e38e3b6377c
Cintya Karina Banda <karibanda10@gmail.com>
2024-02-22 14:26:25 -0600

    Finalización de la pagina de logn

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_set_literal
+
 import "package:flutter/material.dart";
 
 class LoginPage extends StatefulWidget {
@@ -50,13 +52,13 @@ class Contenido extends StatefulWidget {
 class _ContenidoState extends State<Contenido> {
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             'Login',
             style: TextStyle(
               color: Colors.white,
@@ -64,8 +66,8 @@ class _ContenidoState extends State<Contenido> {
               fontSize: 25,
             ),
           ),
-          SizedBox( height: 5, ),
-          Text(
+          const SizedBox(height: 5),
+          const Text(
             "Bienvenido a tu cuenta",
             style: TextStyle(
               color: Colors.white,
@@ -73,12 +75,25 @@ class _ContenidoState extends State<Contenido> {
               letterSpacing: 1.5,
             ),
           ),
-          Datos()
+          const SizedBox(height: 10),
+          const Datos(),
+          const SizedBox(height: 30),
+          Container(
+            alignment: Alignment.center,
+            child: TextButton(
+              onPressed: () => {},
+              child: const Text(
+                "Aviso de Privacidad",
+                style: TextStyle(fontSize: 14),
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
 
 class Datos extends StatefulWidget {
   const Datos({super.key});
@@ -88,10 +103,15 @@ class Datos extends StatefulWidget {
 }
 
 class _DatosState extends State<Datos> {
+  bool showPassword = true;
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+      ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -122,19 +142,138 @@ class _DatosState extends State<Datos> {
           ),
           const SizedBox(height: 5,),
           TextFormField(
-            obscureText: true,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
+            obscureText: showPassword,
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
               hintText: "password",
               suffixIcon: IconButton(
-                icon: Icon(Icons.remove_red_eye_outlined),
-                onPressed: () => {},
-              )                         
+                icon: const Icon(Icons.remove_red_eye_outlined),
+                onPressed: () => {
+                    setState(() {
+                      showPassword == true ? showPassword = false : showPassword = true;
+                    })                
+                },
+              )
             ),
           ),
           const SizedBox(height: 5,),
-        ],
+          const Remember(),
+          const SizedBox( height: 5, ),
+          const Botones(),
+        ],        
       ),
+    );
+  }
+}
+
+class Remember extends StatefulWidget {
+  const Remember({super.key});
+
+  @override
+  State<Remember> createState() => _RememberState();
+}
+
+class _RememberState extends State<Remember> {
+  bool checked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(
+          value: checked,
+          onChanged: (checked) => {
+            setState(()=>checked == false ? checked = true : checked = false
+            )
+          },
+        ),
+        const Text(
+          "Recordar cuenta",
+          style: TextStyle(fontSize: 12,),
+        ),
+        const Spacer(),
+        TextButton(
+          onPressed: ()=>{},
+          child: const Text(
+            "¿Olvido su contraseña?",
+            style: TextStyle(fontSize: 12,),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Botones extends StatelessWidget {
+  const Botones({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: 50,
+          child: ElevatedButton(
+            onPressed: ()=>{},
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(const Color(0xff142047))
+            ),
+            child: const Text(
+              "Login",
+              style: TextStyle(
+                color: Colors.white
+              ),
+            ),            
+          ),
+        ),
+        const SizedBox(
+          height: 25,
+          width: double.infinity,
+        ),
+        const Text(
+          "O entra con:",
+          style: TextStyle(color: Colors.grey,),
+        ),
+        const SizedBox(
+          height: 25,
+          width: double.infinity,
+        ),
+        SizedBox(
+          width: double.infinity,
+          height: 50,
+          child: OutlinedButton(
+            onPressed: ()=>{},
+            child: const Text(
+              "Google",
+              style: TextStyle(
+                color: Color(0xff142047),
+                fontWeight: FontWeight.bold,
+                fontSize: 18
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+          width: double.infinity,
+        ),
+        SizedBox(
+          width: double.infinity,
+          height: 50,
+          child: OutlinedButton(
+            onPressed: ()=>{},
+            child: const Text(
+              "Github",
+              style: TextStyle(
+                color: Color(0xff142047),
+                fontWeight: FontWeight.bold,
+                fontSize: 18
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(),
+      ],
     );
   }
 }
